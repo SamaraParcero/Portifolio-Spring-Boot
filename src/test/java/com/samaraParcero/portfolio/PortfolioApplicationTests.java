2631b6a9477a5c7ef3a4d9e3296808b9dfb609d9
SamaraParcero <samaraparceroti@gmail.com>
2024-05-28 17:39:15 -0300

    projeto criado

---
+++ src/test/java/com/samaraParcero/portfolio/PortfolioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.samaraParcero.portfolio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortfolioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
