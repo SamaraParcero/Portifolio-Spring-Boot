a9ed22844d7c6bee3cee5a8a753adb4cca41b579
SamaraParcero <samaraparceroti@gmail.com>
2024-06-01 17:52:27 -0300

    JPA Repository, dependency injection, database seeding

---
+++ src/main/java/com/samaraParcero/portfolio/repositories/UserRepository.java
@@ -0,0 +1,9 @@
+package com.samaraParcero.portfolio.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.samaraParcero.portfolio.entities.User;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+}
