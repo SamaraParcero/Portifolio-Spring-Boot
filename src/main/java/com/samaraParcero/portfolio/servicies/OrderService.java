5346176a23851278ef21b81a808bc53b677fcb73
SamaraParcero <samaraparceroti@gmail.com>
2024-07-07 22:44:24 -0300

    Order, instant e iso8601 parte 2

---
+++ src/main/java/com/samaraParcero/portfolio/servicies/OrderService.java
@@ -0,0 +1,28 @@
+package com.samaraParcero.portfolio.servicies;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.samaraParcero.portfolio.entities.Order;
+import com.samaraParcero.portfolio.repositories.OrderRepository;
+
+@Service
+public class OrderService {
+	
+	@Autowired
+	private OrderRepository repository;
+	
+	public List<Order> findAll(){
+		return repository.findAll();
+	}
+
+	public Order findById(Long id) {
+		Optional<Order> obj = repository.findById(id);
+		return obj.get();
+	}
+
+
+}
