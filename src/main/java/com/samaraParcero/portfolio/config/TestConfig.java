0b9f055642154555444db935d13eb3a4218a3a97
SamaraParcero <samaraparceroti@gmail.com>
2024-07-09 17:59:35 -0300

    payment one to one

--- src/main/java/com/samaraParcero/portfolio/config/TestConfig.java
+++ src/main/java/com/samaraParcero/portfolio/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.samaraParcero.portfolio.entities.Category;
 import com.samaraParcero.portfolio.entities.Order;
 import com.samaraParcero.portfolio.entities.OrderItem;
+import com.samaraParcero.portfolio.entities.Payment;
 import com.samaraParcero.portfolio.entities.Product;
 import com.samaraParcero.portfolio.entities.User;
 import com.samaraParcero.portfolio.entities.enums.OrderStatus;
@@ -80,6 +81,11 @@ public class TestConfig implements CommandLineRunner{
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice()); 
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+		
+		orderRepository.save(o1);
 	}
 
 }
