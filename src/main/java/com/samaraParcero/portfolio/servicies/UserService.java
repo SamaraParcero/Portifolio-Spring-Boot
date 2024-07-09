f9c06727bf18ef041cc8e803e5dceed5f361ba10
SamaraParcero <samaraparceroti@gmail.com>
2024-07-09 19:24:59 -0300

    tratamento de exceção find by id

--- src/main/java/com/samaraParcero/portfolio/servicies/UserService.java
+++ src/main/java/com/samaraParcero/portfolio/servicies/UserService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import com.samaraParcero.portfolio.entities.User;
 import com.samaraParcero.portfolio.repositories.UserRepository;
+import com.samaraParcero.portfolio.servicies.exceptions.ResourceNotFoundException;
 
 
 @Service
@@ -22,7 +23,7 @@ public class UserService {
 
 	public User findById(Long id) {
 		Optional<User> obj = repository.findById(id);
-		return obj.get();
+		return obj.orElseThrow(()-> new ResourceNotFoundException(id));
 	}
 	
 	public User insert(User obj) {
